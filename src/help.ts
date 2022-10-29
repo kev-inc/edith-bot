7b19847ce5ae1492112da934fc67f03bac6195cc
Kevin <kevinchanxy@gmail.com>
2022-10-29 13:17:34 +0000

    Add keyboard

--- src/help.ts
+++ src/help.ts
@@ -1,8 +1,15 @@
-import { sendTelegramMessage } from "./telegram"
+import { sendTelegramMessage, sendTelegramMessageWithMenu } from "./telegram"
 
 export const genHelpMessage = async (chatId: string) => {
     const message = '*Welcome\\. I am EDITH\\.*\n' +
     '/tasks \\- _Get your list of open tasks_\n' + 
     '/closedtasks \\- _Get your list of closed tasks_\n'
-    await sendTelegramMessage(chatId, message);
+    const menu = {
+        keyboard: [
+          ["View Tasks"],
+          ["Create New Task"],
+          ["Create New Reminder"],
+        ],
+      };
+    await sendTelegramMessageWithMenu(chatId, message, menu)
 }
--- src/telegram.ts
+++ src/telegram.ts
@@ -2,9 +2,20 @@ let API_KEY: string
 
 export const setApiKey = (key: string) => API_KEY = key
 
+export const sendTelegramMessageWithMenu = async (chatId: string, message: string, menu: any) => {
+  const searchParams = new URLSearchParams({
+    chat_id: chatId,
+    text: message,
+    parse_mode: "MarkdownV2",
+    reply_markup: decodeURIComponent(JSON.stringify(menu))
+  })
+  const url = `https://api.telegram.org/bot${API_KEY}/sendMessage?` + searchParams;
+  return await sendMessage(url);
+}
+
 export const sendTelegramMessage = async (chatId: string, message: string, keyboard: any = null) => {
   const searchParams = new URLSearchParams({
-    chat_id: chatId, 
+    chat_id: chatId,
     text: message,
     parse_mode: "MarkdownV2"
   })
@@ -12,15 +23,13 @@ export const sendTelegramMessage = async (chatId: string, message: string, keybo
     searchParams.append("reply_markup", decodeURIComponent(JSON.stringify(keyboard)))
   }
   const url = `https://api.telegram.org/bot${API_KEY}/sendMessage?` + searchParams;
-  console.log(url);
-  const data = await fetch(url).then((resp) => resp.json());
-  return data;
+  return await sendMessage(url);
 };
 
 export const editTelegramMessage = async (chatId: string, messageId: string, message: string, keyboard: any = null) => {
   const searchParams = new URLSearchParams({
-    chat_id: chatId, 
-    message_id: messageId, 
+    chat_id: chatId,
+    message_id: messageId,
     text: message,
     parse_mode: "MarkdownV2"
   })
@@ -28,6 +37,10 @@ export const editTelegramMessage = async (chatId: string, messageId: string, mes
     searchParams.append("reply_markup", decodeURIComponent(JSON.stringify(keyboard)))
   }
   const url = `https://api.telegram.org/bot${API_KEY}/editMessageText?` + searchParams;
+  return await sendMessage(url)
+}
+
+export const sendMessage = async (url: string) => {
   console.log(url);
   const data = await fetch(url).then((resp) => resp.json());
   return data;
