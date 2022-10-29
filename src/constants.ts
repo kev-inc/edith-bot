445420b123595421a273f5739ffe4ae1c7b29a5a
Kevin <kevinchanxy@gmail.com>
2022-10-29 13:48:26 +0000

    tasks

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,9 @@ export const VIEW_TASKS = "✅ My Tasks"
 export const NEW_TASK = "🆕 Create New Task"
 export const NEW_REMINDER = "🔔 Create New Reminder"
 
+export const ASK_FOR_TASK = "Please enter the task"
+export const ASK_FOR_REMINDER = "Please enter the reminder"
+
 export const MENU_KEYBOARD = [
     [VIEW_TASKS],
     [NEW_TASK],
