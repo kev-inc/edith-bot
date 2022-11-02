7fc3a705c397aee99fadaedf0a21078ebbc2ff74
Kevin <kevinchanxy@gmail.com>
2022-11-02 06:24:16 +0000

    ddd

--- src/vm.ts
+++ src/vm.ts
@@ -10,11 +10,12 @@ export const genVM = async (chatId: string) => {
     const resp = await fetch('https://api.github.com/repos/btxkev3/Windows2019RDP-US-MMDYT/actions/workflows/RDP-US.yml/dispatches', {
         method: 'POST', 
         headers: {
+            'content-type': 'application/vnd.github+json',
             'Accept': 'application/vnd.github+json',
             'Authorization': `Bearer ${GH_KEY}`
         },
         body: JSON.stringify({"ref": "main"})
     })
-    console.log(JSON.stringify(resp.body))
+    console.log(JSON.stringify(await resp.json()))
     await sendTelegramMessage(chatId, `Status is ${resp.status}`)
 }
